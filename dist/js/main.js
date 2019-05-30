9bdcf1eff4e0e5e8a2cfbe2571d2330cc249e74d
theworldofjin <theworldofjin@gmail.com>
2019-05-30 16:15:43 -0400

    Divided into sections

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const homePage = document.querySelector("#homepage");
 
 // Set Initial State Of Menu
 let showMenu = false;
@@ -16,20 +17,24 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
+    homePage.classList.add("hide");
     navItems.forEach(item => item.classList.add("show"));
 
-    menuBtn.textContent = "MAIN";
-    menuBtn.style.color = "#CD1300";
+    menuBtn.textContent = "CLOSE MENU";
+    menuBtn.style.color = "#000";
+    homePage.style.visibility = "hidden";
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
+    homePage.classList.remove("hide");
     navItems.forEach(item => item.classList.remove("show"));
 
-    menuBtn.textContent = "HOME";
+    menuBtn.textContent = "SHOW MENU";
     menuBtn.style.color = "#FFF";
+    homePage.style.visibility = "visible";
     showMenu = false;
   }
 }
