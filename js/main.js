5417a1f15eda0a04b41123a47e90da4c25dc04d4
Jinman Kim <theworldofjin@gmail.com>
2019-06-07 22:16:06 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  backSpeed: 50,
+  typeSpeed: 50,
+  loop: true
+});
+
+AOS.init({
+  duration: 1500,
+  once: "true",
+  easing: "ease-out"
+});
